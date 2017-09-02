de17f7706434b81f309d2815c7c4f7cf3053149b
Ricardo Mendes <rokusu@gmail.com>
2017-09-02 10:51:11 +0100

    Fix path to package.json

--- lib/ember/version.rb
+++ lib/ember/version.rb
@@ -3,7 +3,7 @@ require 'json'
 module Ember
   extend self
 
-  package = File.read(File.expand_path('../../../../package.json', __FILE__))
+  package = File.read(File.expand_path('../../../package.json', __FILE__))
 
   VERSION = JSON.parse(package)['version'].strip.gsub(/[-\+]/, '.')
 end
